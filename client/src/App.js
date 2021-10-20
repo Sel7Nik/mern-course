2db79d04e9def487c21767d404bfbfdee391d046
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-21 00:46:02 +0300

    1:05:24 Router Switch, Route, Redirect

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import 'materialize-css'
+import { useRoutes } from './routes'
+import { BrowserRouter as Router } from 'react-router-dom'
 
+//?
 function App() {
+  const routes = useRoutes(false)
   return (
-    <div className="container">
-      <h1> App page</h1>
-    </div>
+    <Router>
+      <div className="container">
+        <h1> App page</h1>
+        {routes}
+      </div>
+    </Router>
   )
 }
 
