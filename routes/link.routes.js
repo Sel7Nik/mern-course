365b39aef9bab84c5d621a562e04f4fe37bb32a7
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-10-21 23:25:38 +0300

    2:25:10   link.routes.js

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -1,6 +1,38 @@
 const { Router } = require('express')
+const config = require('config')
+const shortid = require('shortid')
 const Link = require('../models/Link')
+const auth = require('../middleware/auth.middleware')
 const router = Router()
+
+//!--- router.post
+router.post('/generate', auth, async (req, res) => {
+  try {
+    const baseUrl = config.get('baseUrl')
+    const { from } = req.body
+
+    const code = shortid.generate()
+
+    const existing = await Link.findOne({ from })
+
+    if (existing) {
+      return res.json({ link: existing })
+    }
+
+    const to = baseUrl + '/t/' + code
+
+    const link = new Link({
+      code,
+      to,
+      from,
+      owner: req.user.userId,
+    })
+
+    await link.save()
+
+    res.status(201).json({ link })
+  } catch {}
+})
 
 //!--- router.get
 router.get('/', auth, async (req, res) => {
